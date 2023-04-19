a55e4966e8016f8971c373209a634833625e5969
Doyun <doyun@talktomeinkorean.com>
2023-04-19 11:54:40 +0900

    style : hover scale up 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import logo from "./logo.svg";
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import BlogPost from "./components/BlogPost";
 
 function App() {
   return (
     <Router>
-      <nav className='flex items-center justify-center py-3 border-b-2'>
-        <img src={logo} className='w-10' alt='logo' />
-        <p className='text-xl font-noto font-[500] mr-10'>DevStoryBlog</p>
+      <nav className="flex items-center justify-center py-3 border-slate-900 border-b-2">
+        <img src={logo} className="w-10" alt="logo" />
+        <p className="text-xl font-noto font-[500] mr-10">DevStoryBlog</p>
       </nav>
       <Routes>
-        <Route path='/DevStoryBlog' element={<Home />} />
-        <Route path='/post/:postID' element={<BlogPost />} />
+        <Route path="/DevStoryBlog" element={<Home />} />
+        <Route path="/post/:postID" element={<BlogPost />} />
       </Routes>
     </Router>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,26 +3,32 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
+    <div className="mx-10">
       <Link to="/post/1">
-        <div className="mx-10 my-5 px-5 py-7 border-black border-[0px] rounded-lg bg-[#f8C44c] shadow-md transition ease-in-out delay-100  hover:-translate-y-0 hover:scale-105 hover: duration-300">
-          <h1 className="text-2xl  font-[300]">Shadow DOM 이란?</h1>
-          <p className="text-sm font-nanum">Shadow DOM이란 무엇일까요? ...</p>
+        <div className="my-5 px-5 py-7 border-slate-900 border-2 rounded-lg bg-[#f8C44c] shadow-md transition ease-in-out delay-100  hover:-translate-y-0 hover:bg-white hover:shadow-2xl duration-300">
+          <h1 className="text-2xl font-semibold italic text-slate-900 text-center mb-3">
+            ¿Qué es DataDog?
+          </h1>
+          <p className="text-sm text-slate-800 font-nanum">¿Qué es DataDog?</p>
         </div>
       </Link>
       <Link to="/post/2">
-        <div className="mx-10 my-5 px-5 py-7 border-black border-[0px] rounded-lg bg-[#3ebd5c] shadow-md transition ease-in-out delay-100  hover:-translate-y-0 hover:scale-105 hover: duration-300">
-          <h1 className="text-lg font-[300]">Shadow DOM 이란?</h1>
+        <div className="my-5 px-5 py-7 border-slate-900 border-2 rounded-lg bg-[#3ebd5c] shadow-md transition ease-in-out delay-100  hover:-translate-y-0 hover:shadow-2xl hover:bg-white  duration-300">
+          <h1 className="text-2xl font-semibold italic text-slate-900 text-center mb-3">
+            Shadow DOM 이란?
+          </h1>
           <p className="text-sm font-nanum">Shadow DOM이란 무엇일까요? ...</p>
         </div>
       </Link>
       <Link to="/post/3">
-        <div className="mx-10 my-5 px-5 py-7 border-black border-[0px] rounded-lg bg-[#428fff] shadow-md transition ease-in-out delay-100  hover:-translate-y-0 hover:scale-105 hover: duration-300">
-          <h1 className="text-lg font-[300]">Shadow DOM 이란?</h1>
+        <div className="my-5 px-5 py-7 border-slate-900 border-2 rounded-lg bg-[#428fff] shadow-md transition ease-in-out delay-100  hover:-translate-y-0 hover:shadow-2xl hover:bg-white  duration-300">
+          <h1 className="text-2xl font-semibold italic text-slate-900 text-center mb-3">
+            Shadow DOM 이란?
+          </h1>
           <p className="text-sm font-nanum">Shadow DOM이란 무엇일까요? ...</p>
         </div>
       </Link>
-    </>
+    </div>
   );
 };
 
